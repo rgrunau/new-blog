9615d5a364b07e25f2742f0d64dcad0e816c7a8d
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-04-30 20:07:14 -0700

    move posts into content collection

--- src/pages/rss.xlm.ts
+++ src/pages/rss.xlm.ts
@@ -1,12 +1,21 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: any) {
+  const posts = await getCollection("posts");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    //@ts-ignore
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    //@ts-ignore
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
