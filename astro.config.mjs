1190018d92f033bbd02103f9191ff1ed6957e830
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-05-05 15:18:01 -0700

    next set of changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), solidJs()]
+});
