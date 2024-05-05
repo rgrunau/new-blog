ce091bd03d6d819707f13b052ce3625ac8268fa0
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-05-05 16:27:38 -0700

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), solidJs()]
+  site: "https://robertgrunau.website",
+  integrations: [mdx(), solidJs()],
 });
