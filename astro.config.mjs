7480235b7dd7ba28c46c9b74c384094a8b950b9b
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-05-05 21:48:27 -0700

    add site mapping

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://robertgrunau.website",
-  integrations: [mdx(), solidJs()],
+  integrations: [mdx(), solidJs(), sitemap()]
 });
