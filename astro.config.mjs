d3e882511921e287a68224a3e738af928093a6ac
Robert Grunau <robertgrunau@hey.com>
2024-08-06 20:42:46 -0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://robertgrunau.website",
-  integrations: [mdx(), solidJs(), sitemap()]
+  integrations: [mdx(), solidJs(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
